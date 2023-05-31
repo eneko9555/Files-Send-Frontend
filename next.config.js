e47f4133b1772cfeec7472cba8258ae47a6687bc
eneko9555 <eneko_55@hotmail.com>
2023-05-31 11:30:58 +0200

    first_commit

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        BACKEND_URL: "http://localhost:4000/api",
-        FRONTEND_URL: "http://localhost:3000"
-    },
-}
-
-module.exports = nextConfig
