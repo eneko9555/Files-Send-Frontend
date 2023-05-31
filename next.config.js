2b77d0a56f639056eddcb2bf92cfc7d37d450ddd
eneko9555 <eneko_55@hotmail.com>
2023-05-31 12:24:29 +0200

    first_commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        BACKEND_URL: "https://files-send-backend.onrender.com/api",
+        FRONTEND_URL: "https://incredible-babka-3ae75e.netlify.app"
+    },
+}
+
+module.exports = nextConfig
