3aa890f35a3a8d751c9a0a779cac03b8c9329d22
eneko9555 <eneko_55@hotmail.com>
2023-06-01 11:25:30 +0200

    first-commit

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
+ 
  const nextConfig = {
-    
+     env: {
+         BACKEND_URL: "https://files-send-backend-production.up.railway.app/api",
+         FRONTEND_URL: "https:files-send-frontend.vercel.app"
+     },
  }
-
 
 
 module.exports = nextConfig
 
 
-// const nextConfig = {
-//     env: {
-//         BACKEND_URL: "https://files-send-backend.onrender.com/api",
-//         FRONTEND_URL: "https://files-send-frontend.vercel.app"
-//     },
-// }
